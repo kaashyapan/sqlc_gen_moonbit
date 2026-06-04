4bd68995871b972c7bdca92471fa3b564b336233
kaashyapan <sunder.narayanaswamy@gmail.com>
2026-06-04 22:02:47 +0530

    Rewrite postgres native using moonbit-community/postgres

--- examples/postgres_native/db/postgres/query.sql
+++ examples/postgres_native/db/postgres/query.sql
@@ -1,14 +1,20 @@
 -- name: GetUser :one
-SELECT * FROM users WHERE id = $1;
+SELECT * FROM test_users WHERE id = $1;
+
+-- name: GetByDate :many
+SELECT * FROM test_users WHERE date_var = $1;
+
+-- name: GetByUuid :many
+SELECT * FROM test_users WHERE uuid_var = $1;
 
 -- name: ListUsers :many
-SELECT * FROM users ORDER BY id;
+SELECT * FROM test_users ORDER BY id;
 
 -- name: CreateUser :execlastid
-INSERT INTO users (name, email) VALUES ($1, $2) RETURNING id;
+INSERT INTO test_users (name, email, uuid_var, date_var) VALUES ($1, $2, $3, $4) RETURNING id;
 
 -- name: UpdateUser :execrows
-UPDATE users SET name = $2, email = $3 WHERE id = $1;
+UPDATE test_users SET name = $2, email = $3, date_var = $4 WHERE id = $1;
 
 -- name: DeleteUser :exec
-DELETE FROM users WHERE id = $1;
+DELETE FROM test_users WHERE id = $1;
--- examples/postgres_native/db/postgres/schema.sql
+++ examples/postgres_native/db/postgres/schema.sql
@@ -1,6 +1,12 @@
-CREATE TABLE IF NOT EXISTS users (
+--DROP TABLE test_users ;
+
+CREATE TABLE IF NOT EXISTS test_users (
     id SERIAL PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
-    email VARCHAR(255) UNIQUE NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    email VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    "uuid_var" uuid,
+    "date_var" date NOT NULL
 );
+
+-- INSERT INTO "public"."test_users" ("id", "name", "email", "created_at", "uuid_var", "date_var") VALUES (1, 'Elon', 'elon@tesla.com', '2026-06-01 01:49:43.664991', '019dc341-9b48-77e4-806a-7a303d5395f5', '2026-06-01');
